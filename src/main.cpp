69f879d25fcf320ca810d94c76853e32b2048148
Kris131 <bacaludavid@yahoo.com>
2025-05-15 20:45:34 +0300

    Testing LCD, button and buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+// #include <Keypad.h>
+
+// initialize pins
+#define BUTTON_PIN PC3 // A3 pin on Arduino Uno
+#define BUZZER_PIN 10
+
+volatile bool buttonPressed = false;
+
+// initialize LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int wrongSound = 300;
+const int rightSound = 1000;
+const int buzzerDuration = 200;
 
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, World!");
+
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  DDRC &= ~(1 << BUTTON_PIN); // Set BUTTON_PIN as input
+  PORTC |= (1 << BUTTON_PIN); // Enable pull-up resistor
+
+  PCICR |= (1 << PCIE1); // Enable pin change interrupt for PCINT0
+  PCMSK1 |= (1 << PCINT11); // Enable interrupt for BUTTON_PIN
+  sei(); // Enable global interrupts
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (buttonPressed) {
+    tone(BUZZER_PIN, wrongSound, buzzerDuration);
+    buttonPressed = false;
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ISR(PCINT1_vect) {
+  if (!(PINC & (1 << BUTTON_PIN))) { // Check if button is pressed
+    buttonPressed = true;
+    delay(50); // Debounce delay
+  }
 }
