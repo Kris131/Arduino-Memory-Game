49b3cae58098671f3afb4c6cb9648f8905226d4d
Kris131 <bacaludavid@yahoo.com>
2025-05-15 21:28:50 +0300

    Added idle animation to test LEDs.

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,19 @@
 #define BUTTON_PIN A3
 #define BUTTON_BIT PC3 // A3 pin on Arduino Uno
 #define BUZZER_PIN 10
+#define SR_DATA_PIN 11
+#define SR_LATCH_PIN 12
+#define SR_CLK_PIN 13
 
 volatile bool buttonPressed = false;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
+
+
+volatile bool idleState = true;
+int ledIndex = 0;
+unsigned long lastLedUpdateTime = 0;
+const unsigned long ledInterval = 500; // 500ms per LED
 
 // initialize LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -34,6 +43,7 @@ const int rightSound = 1000;
 const int buzzerDuration = 200;
 
 // put function declarations here:
+void sendToShiftRegister(uint16_t data);
 
 void setup() {
   // put your setup code here, to run once:
@@ -50,6 +60,11 @@ void setup() {
   PCICR |= (1 << PCIE1); // Enable pin change interrupt for PCINT0
   PCMSK1 |= (1 << PCINT11); // Enable interrupt for BUTTON_BIT
   sei(); // Enable global interrupts
+
+  // Initialize the shift register pins
+  pinMode(SR_DATA_PIN, OUTPUT);
+  pinMode(SR_LATCH_PIN, OUTPUT);
+  pinMode(SR_CLK_PIN, OUTPUT);
 }
 
 void loop() {
@@ -71,10 +86,28 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print(key);
   }
+
+  if (idleState) {
+    unsigned long currentTime = millis();
+    if (currentTime - lastLedUpdateTime >= ledInterval) {
+      uint16_t data = 1 << ledIndex;
+      sendToShiftRegister(data);
+      ledIndex = (ledIndex + 1) % 9;
+      lastLedUpdateTime = currentTime;
+    }
+  }
 }
 
 ISR(PCINT1_vect) {
   if (!(PINC & (1 << BUTTON_BIT))) { // Check if button is pressed
     buttonPressed = true;
   }
+}
+
+// functions
+void sendToShiftRegister(uint16_t data) {
+  digitalWrite(SR_LATCH_PIN, LOW); // Set latch low to start sending data
+  shiftOut(SR_DATA_PIN, SR_CLK_PIN, MSBFIRST, highByte(data)); // Send high byte
+  shiftOut(SR_DATA_PIN, SR_CLK_PIN, MSBFIRST, lowByte(data)); // Send low byte
+  digitalWrite(SR_LATCH_PIN, HIGH); // Set latch high to finish sending data
 }
