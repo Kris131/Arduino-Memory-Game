82bdf5a4fc143c67ef8c1bf2cd7586d404fc471b
Kris131 <bacaludavid@yahoo.com>
2025-05-17 15:02:48 +0300

    Added functional easy mode.

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,8 @@ const unsigned long debounceDelay = 50;
 volatile bool idleState = true;
 int ledIndex = 0;
 unsigned long lastLedUpdateTime = 0;
-const unsigned long ledInterval = 500; // 500ms per LED
+const unsigned long ledInterval = 500;
+int difficulty = 0;
 
 // initialize LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -42,15 +43,24 @@ const int wrongSound = 300;
 const int rightSound = 1000;
 const int buzzerDuration = 200;
 
+// initialize the easy/hard difficulty arrays
+const int minLength = 4;
+int currLength = 4;
+int highScore = 0;
+uint16_t easy[32];
+uint16_t hard[32];
+
 // put function declarations here:
 void sendToShiftRegister(uint16_t data);
+void printIdleMessage();
+void printWrongMessage();
+void printCorrectMessage();
 
 void setup() {
   // put your setup code here, to run once:
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello, World!");
+  printIdleMessage();
 
   pinMode(BUZZER_PIN, OUTPUT);
 
@@ -65,26 +75,97 @@ void setup() {
   pinMode(SR_DATA_PIN, OUTPUT);
   pinMode(SR_LATCH_PIN, OUTPUT);
   pinMode(SR_CLK_PIN, OUTPUT);
+
+  sendToShiftRegister(0);
+
+  for (int i = 0; i < currLength; i++) {
+    easy[i] = random(0, 8);
+    hard[i] = random(0, 8);
+  }
 }
 
 void loop() {
+  if (!idleState) {
+    for (int i = 0; i < currLength; i++) {
+      sendToShiftRegister(0);
+      delay(300);
+      sendToShiftRegister(1 << easy[i]);
+      delay(300);
+    }
+    sendToShiftRegister(0);
+
+    for (int i = 0; i < currLength; i++) {
+      char keyPressed = keypad.waitForKey();
+      uint16_t keyInt = keyPressed - '0';
+
+      if (keyInt != easy[i] + 1) {
+        tone(BUZZER_PIN, wrongSound, buzzerDuration);
+        printWrongMessage();
+
+        currLength = minLength;
+
+        for (int j = 0; j < currLength; j++) {
+          easy[j] = random(0, 8);
+        }
+
+        idleState = true;
+        ledIndex = 0;
+        sendToShiftRegister(0);
+        break;
+      }
+
+      if (i == currLength - 1) {
+        tone(BUZZER_PIN, rightSound, buzzerDuration);
+        printCorrectMessage();
+
+        if (currLength > highScore) {
+          highScore = currLength;
+        }
+
+        currLength++;
+        easy[currLength - 1] = random(0, 8);
+
+        break;
+      }
+    }
+
+    return;
+  }
+
   // put your main code here, to run repeatedly:
   if (buttonPressed) {
     unsigned long currentTime = millis();
     if (currentTime - lastDebounceTime > debounceDelay) {
-      tone(BUZZER_PIN, wrongSound, buzzerDuration);
+      difficulty = 1 - difficulty;
+      printIdleMessage();
+      lcd.setCursor(0, 1);
+      lcd.print("Difficulty: ");
+      lcd.print(difficulty ? "Easy" : "Hard");
+      // tone(BUZZER_PIN, wrongSound, buzzerDuration);
       lastDebounceTime = currentTime;
+
+      currLength = minLength;
+      for (int i = 0; i < currLength; i++) {
+        easy[i] = random(0, 8);
+        hard[i] = random(0, 8);
+      }
     }
     buttonPressed = false;
   }
 
   char key = keypad.getKey();
-  if (key) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("You pressed:");
-    lcd.setCursor(0, 1);
-    lcd.print(key);
+  // if (key) {
+  //   lcd.clear();
+  //   lcd.setCursor(0, 0);
+  //   lcd.print("You pressed:");
+  //   lcd.setCursor(0, 1);
+  //   lcd.print(key);
+  // }
+
+  if (key == '0') {
+    idleState = !idleState;
+    ledIndex = 0;
+    sendToShiftRegister(0);
   }
 
   if (idleState) {
@@ -111,3 +192,46 @@ void sendToShiftRegister(uint16_t data) {
   shiftOut(SR_DATA_PIN, SR_CLK_PIN, MSBFIRST, lowByte(data)); // Send low byte
   digitalWrite(SR_LATCH_PIN, HIGH); // Set latch high to finish sending data
 }
+
+void printIdleMessage() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Press 0 to start");
+  lcd.setCursor(0, 1);
+  lcd.print("HS: ");
+  lcd.print(highScore);
+}
+
+void printWrongMessage() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Wrong! Press 0 to play again");
+  lcd.setCursor(0, 1);
+  lcd.print("HS: ");
+  lcd.print(highScore);
+  lcd.print(" ");
+  lcd.print("S: ");
+  lcd.print(currLength - 1);
+}
+
+void printCorrectMessage() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Correct! Next lvl");
+  lcd.setCursor(0, 1);
+  lcd.print("HS: ");
+  lcd.print(highScore);
+  lcd.print(" ");
+  lcd.print("S: ");
+  lcd.print(currLength);
+}
+
+/*
+TODO: make the user watch the sequence
+print on lcd 'Start'
+the user will input n numbers
+check the input with the sequence
+if an input is wrong, play a sound and a message on lcd with 'press 0 to start again'
+if the input is correct, play a sound and print 'correct' and add 1 to the sequence
+if easy add one led, if hard redo the sequence with new numbers with one more led
+*/
